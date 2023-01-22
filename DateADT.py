c6ff286de5d1476eb2a56cc92b813d60c09bcfdd
Maryam Panjri <122341923+Maryam0330@users.noreply.github.com>
2023-01-22 19:56:36 +0530

    Create DateADT.py
    This is a proleptic Gregorian calendar which shows you the day, month and year including day of the week and also shows whether the year is a leap year or not. This code is written using the Python language.

---
+++ DateADT.py
@@ -0,0 +1,27 @@
+Date ADT
+import datetime
+class DateADT:
+    def __init__(self,date,month,year):
+        self.date=date
+        self.month=month
+        self.year=year
+    def show_date(self):
+        print("Date:",self.date,"/",self.month,"/",self.year)
+    def show_day(self):
+        return self.date
+    def show_month(self):
+        return self.month
+    def show_year(self):
+        return self.year
+    def isLeapYear(self):
+        return (self.year%4==0)
+    def dayOfWeek(self):
+               dat=datetime.date(self.year,self.month,self.date)
+        return dat.strftime("%A")
+mydate=DateADT(10,1,23)
+mydate.show_date()
+print("Day is",mydate.show_day())
+print("Month is",mydate.show_month())
+print("Year is",mydate.show_year())
+print("Is it a leap year?",mydate.isLeapYear())
+print("Day of the week is",mydate.dayOfWeek())
