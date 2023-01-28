fe3c0c4c6c8380320c0d159002603bf8d94e3250
Maryam Panjri <122341923+Maryam0330@users.noreply.github.com>
2023-01-28 17:21:52 +0530

    Create email_slicer.py

---
+++ email_slicer.py
@@ -0,0 +1,20 @@
+#email slicer using python
+#collect email from user
+#split the email using @, the first part as the username, the second part as as domain
+#split domain using .
+
+def main():
+  print("Welcome to the email slicer")
+  print("")
+  
+  email_input = input("Input your email address: ")
+  
+  (username, domain) = email_input.split("@")
+  (domain, extension) = domain.split(".")
+  
+  print("Username: ",username)
+  print("Domain: ", domain)
+  print("Extension: ", extension)
+
+while True: 
+  main()
