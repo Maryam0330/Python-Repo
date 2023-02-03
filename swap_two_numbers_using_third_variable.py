29a9ab97e75c82ec728c2a2cef1d37fffeb48bc9
Maryam Panjri <122341923+Maryam0330@users.noreply.github.com>
2023-02-03 23:44:47 +0530

    Create swap_two_numbers_using_third_variable.py

---
+++ swap_two_numbers_using_third_variable.py
@@ -0,0 +1,11 @@
+#swap two numbers using third variable in python
+a=int(input("Enter value of a: "))
+b=int(input("Enter value of b: "))
+
+print("\nBefore swapping \na =",a,"\nb =",b)
+
+temp=a
+a=b
+b=temp
+
+print("\nAfter swapping \na =",a,"\nb =",b)
