03d5c4fa43a598bf194466dff58fb10feb087258
Maryam Panjri <122341923+Maryam0330@users.noreply.github.com>
2023-02-03 23:47:06 +0530

    Create swap_two_numbers_without_using_third_variable.py

---
+++ swap_two_numbers_without_using_third_variable.py
@@ -0,0 +1,11 @@
+#swap two numbers without using third variable in python
+a=5
+b=6
+
+print("Before swapping \na =",a,"\nb =",b)
+
+a=a+b #11
+b=a-b #5
+a=a-b #6
+
+print("\nAfter swapping \na =",a,"\nb =",b)
